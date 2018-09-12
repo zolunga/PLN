386ed91d8d94f82567ad096d1e0bd3040858818d
root <alangarcia3106@gmail.com>
2018-09-12 13:38:31 -0500

    lemmas corregidos y mas rapido

--- lemma.py
+++ lemma.py
@@ -59,52 +59,69 @@ def hasNumber(Str):
 def cleanTokens3(vocabulario):
     '''Solo limpia el vocabulario, usa una expresion regular para validar letra
     por letra y evitar que se ingresen caracteres no deseados o deconocidos en el corpus'''
-    path = 'lemma.txt'
-    doc = open(path,'w')
     palabra_limpia = "";
+    Arr = []
     for word in vocabulario:
         palabra_limpia = ""
         for letter in word:
             if re.match(r'[a-záéíóúñ]', letter):
                 palabra_limpia += letter
         if palabra_limpia != "" and "www" not in palabra_limpia and "http" not in palabra_limpia:
-            for key in dic_lemmas:
-                if palabra_limpia.startswith(key):
-                    for i in range(0,len(dic_lemmas[key])):
-                        if(palabra_limpia == dic_lemmas[key][0]):
-                            palabra_limpia += ", "
-                            doc.write(palabra_limpia)
-                            break
-                        stringtem = key + dic_lemmas[key][i]
-                        if(palabra_limpia == stringtem):
-                            #print(palabra_limpia, ">>>>>>", dic_lemmas[key][0])
-                            palabra_limpia = dic_lemmas[key][0]
-                            palabra_limpia += ", "
-                            doc.write(palabra_limpia)
-                            break;
+            Arr.append(palabra_limpia)
+    print("cleanTokens terminado")
+    return Arr
+
+def Lemmas(vocabulario):
+    print("------------------------------------------------------------------------------------------------------------------------lematizando")
+    palabra_lemmatizada = ""
+    listEscritura = []
+    for word in vocabulario:
+        palabra_lemmatizada = word
+        dic_temporal = dic_lemmas[word[0]]
+        for key in dic_temporal:
+            if word.startswith(key):
+                for i in range(0,len(dic_temporal[key])):
+                    if(word == dic_temporal[key][0]):
+                        palabra_lemmatizada = dic_temporal[key][0]
+                        break
+                    if dic_temporal[key][i] == "":
+                        continue
+                    stringtem = key + dic_temporal[key][i]
+                    if(word == stringtem):
+                        palabra_lemmatizada = dic_temporal[key][0]
+                        break
+        listEscritura.append(palabra_lemmatizada)
+    file=open("lemma.txt", "w")
+    file.write(" ".join(listEscritura))
+    file.close()
+            
 
 file=open("generate.txt", encoding="latin-1")
 temp=file.read()
 file.close()
 
 lin = temp.split("\n")
+lin = lin[19:]
 dic_lemmas = {}
 for line in lin:
     lineSplit = line.split(" ")
     if len(lineSplit) > 1:
         lemma = lineSplit[0]
         x = lemma.find("#")
-        if lemma[:x] in dic_lemmas:
-            dic_lemmas[lemma[:x]].append(lemma[x+1:])
+        #print(lemma[0])
+        if lemma[0] not in dic_lemmas:
+            dic_lemmas[lemma[0]] = {}
+        if lemma[:x] in dic_lemmas[lemma[0]]:
+            dic_lemmas[lemma[0]][lemma[:x]].append(lemma[x+1:])
         else:
-            dic_lemmas[lemma[:x]] = []
+            dic_lemmas[lemma[0]][lemma[:x]] = []
             try:
                 if lineSplit[-1] != "":
-                    dic_lemmas[lemma[:x]].append(lineSplit[-1])
+                    dic_lemmas[lemma[0]][lemma[:x]].append(lineSplit[-1])
                 else:
-                    dic_lemmas[lemma[:x]].append(lineSplit[-2])
+                    dic_lemmas[lemma[0]][lemma[:x]].append(lineSplit[-2])
             except IndexError:
                 print(x)
-                
 texto = getCorpus('e960401.htm', 'latin-1')
-cleanTokens3(texto)
+texto2 = cleanTokens3(texto)
+Lemmas(texto2)
