63504631f3c93eed62ed0738b447e7f4682b9f10
root <alangarcia3106@gmail.com>
2018-09-21 12:11:34 -0500

    GetArticles

--- cap5.py
+++ cap5.py
@@ -5,6 +5,7 @@ from nltk.corpus import PlaintextCorpusReader
 import operator
 import numpy as np
 import math
+import re
 
 def getCorpus(NameDoc, encode):
     ''' Obtiene los tokens del texto (considerando que ya esta limpio-lemma.py-)'''
@@ -16,7 +17,33 @@ def getCorpus(NameDoc, encode):
     tokens =  nltk.Text(nltk.word_tokenize(tS))
     return tokens
 
+def getRawCorpus(NameDoc, encode):
+    f = open(NameDoc, encoding=encode)
+    t = f.read()
+    f.close()
+    t = t.replace('\x97', ' ')
+    return t
 
+def getArticles(TextE):
+    ArticlesClean = []
+    Articles = re.split(r'http://www.excelsior.com.mx/9604/960401/[\w]{3}[\d]{2}.html',TextE)
+    print(len(Articles))
+    for art in Articles:
+        soup = BeautifulSoup(art, 'lxml')
+        ArticlesClean.append(soup.get_text())
+    return ArticlesClean
+
+RawText = getRawCorpus('e960401.htm', 'latin-1')
+ArticlesL = getArticles(RawText)
+print(ArticlesL[4])
+
+
+
+
+
+
+
+#---------------------------------------------------- CAP
 text = getCorpus('lemma.txt','latin-1')
 textDD = nltk.DefaultTagger('NN')
 patterns = [
@@ -29,9 +56,7 @@ regexp = nltk.RegexpTagger(patterns)
 #regexp.tag(textDD)
 #print( textDD )
 #print( regexp.tag(text) )
-
-
-
 textTagger = nltk.pos_tag(text)
-print(textTagger)
+#print(textTagger)
+#----------------------------------------------------
 
