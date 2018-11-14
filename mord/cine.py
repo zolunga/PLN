2defb560350bbffe8473b15511311b6b5e27e003
alan <alangarcia3106@gmail.com>
2018-11-13 22:48:54 -0600

    cine

--- mord/cine.py
+++ mord/cine.py
@@ -9,18 +9,21 @@ import nltk
 from bs4 import BeautifulSoup
 from xml.dom import minidom
 
-def getRankXML(rawXML):
-    xml = minidom.parseString(rawXML)
-    review = xml.getElementsByTagName('review')
-    return review[0].attributes['rank'].value
-
+def getRankXML(rawXML, nameXML):
+    try:
+        xml = minidom.parseString(rawXML)
+        review = xml.getElementsByTagName('review')
+        return review[0].attributes['rank'].value
+    except Exception as e:
+        print("Lectura fallida en", nameXML, "--", e)
+        return 0
 def getLemmas(text):
     ''' Obtiene los tokens del texto y elimina algunos caracteres'''
     lemmas = ""
     lines = text.split("\n")
     for line in lines:
         try:
-            print((line.split(" "))[1])
+            #print((line.split(" "))[1])
             lemmas += (line.split(" "))[1] + " "
         except:
             continue
@@ -28,25 +31,31 @@ def getLemmas(text):
   
 
 def getTemcorpus(NameDoc, encode):
-    f=open(NameDoc, encoding=encode)
-    t=f.read()
-    f.close()
-    return t
+    try:
+        f=open(NameDoc, encoding=encode)
+        t=f.read()
+        f.close()
+        return t
+    except: 
+        return False
 
 categories = []
 text = []
 
-for i in range(2,4):
+for i in range(2,4000):  #4280
     path1 = 'corpus/' + str(i) + '.xml'
     path2 = 'corpus/' + str(i) + '.review.pos'
     
     rawXML = getTemcorpus(path1,'latin-1')
-    categories.append(getRankXML(rawXML))
-    
     rawText = getTemcorpus(path2,'latin-1')
+    
+    if rawXML == False or rawText == False:
+        continue
+    
+    categories.append(getRankXML(rawXML, path1))
     text.append(getLemmas(rawText))      
 
 
-print(categories)
-print(text)
+#print(categories)
+#print(text)
 
