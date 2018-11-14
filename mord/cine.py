61e390e3a49301a44fdb207ccadd852d812b5223
alan <alangarcia3106@gmail.com>
2018-11-13 23:11:36 -0600

    cine

--- mord/cine.py
+++ mord/cine.py
@@ -5,11 +5,13 @@ Created on Tue Nov 13 15:11:19 2018
 
 @author: alan
 """
-import nltk
+#import mord as m
 from bs4 import BeautifulSoup
 from xml.dom import minidom
+import time
 
 def getRankXML(rawXML, nameXML):
+    rawXML.replace("&", "")
     try:
         xml = minidom.parseString(rawXML)
         review = xml.getElementsByTagName('review')
@@ -31,18 +33,20 @@ def getLemmas(text):
   
 
 def getTemcorpus(NameDoc, encode):
+    
     try:
         f=open(NameDoc, encoding=encode)
         t=f.read()
         f.close()
+        t.replace("&", "")
         return t
     except: 
         return False
 
 categories = []
 text = []
-
-for i in range(2,4000):  #4280
+fails = 0
+for i in range(2,4280):  #4280
     path1 = 'corpus/' + str(i) + '.xml'
     path2 = 'corpus/' + str(i) + '.review.pos'
     
@@ -50,12 +54,13 @@ for i in range(2,4000):  #4280
     rawText = getTemcorpus(path2,'latin-1')
     
     if rawXML == False or rawText == False:
+        fails += 1
         continue
     
     categories.append(getRankXML(rawXML, path1))
     text.append(getLemmas(rawText))      
 
-
+print("Total de errores:", fails)
 #print(categories)
 #print(text)
 
